e41d4be21c97e3f8111363210e6154243139a1f3
Louis Foster <123@louisfoster.com>
2016-10-12 17:59:39 +1100

    distortion filter

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,34 @@ function inOut(input, output) {
   });
 }
 
-let BQFilter = React.createClass({
+let Distortion0 = React.createClass({
+  render: function() {
+    let distort = this.props.context.createWaveShaper();
+    var k = 800,
+      n_samples = 10,
+      curve = new Float32Array(n_samples),
+      deg = Math.PI / (Math.random() * 100),
+      i = 0,
+      x;
+    for ( ; i < n_samples; ++i ) {
+      x = i * 2 / n_samples - 1;
+      curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) ) *
+        Math.random() * 2 * Math.pow(2, Math.random());
+    }
+    distort.curve = curve;
+    distort.oversample = '4x';
+    let inputs = inOut(this.props.children, distort);
+    distort.connect(this.props.out);
+    return (
+      <div>
+        <p>Attached to distortion filter</p>
+        {inputs}
+      </div>
+    );
+  }
+});
+
+let BQFilter0 = React.createClass({
   render: function() {
     let bqf = this.props.context.createBiquadFilter();
     bqf.type = this.props.type;
@@ -16,17 +43,18 @@ let BQFilter = React.createClass({
     bqf.connect(this.props.out);
     return (
       <div>
-        <p>Attached to filter</p>
+        <p>Attached to BQ Filter</p>
         {inputs}
       </div>
     );
   }
 });
 
-let Osc = React.createClass({
+let Osc0 = React.createClass({
   render: function() {
     let osc = this.props.context.createOscillator();
     osc.frequency.value = this.props.freq;
+    osc.type = this.props.type;
     osc.connect(this.props.out);
     osc.start();
     return (
@@ -38,7 +66,7 @@ let Osc = React.createClass({
   }
 })
 
-let Out = React.createClass({
+let Out0 = React.createClass({
   render: function() {
     let dest = this.props.context.destination;
     let inputs = inOut(this.props.children, dest);
@@ -56,11 +84,17 @@ let Context = React.createClass({
     return (
       <div>
         <p>Audio context rendered</p>
-        <Out {...this.props}>
-          <BQFilter {...this.props} type='lowshelf' freq={100} gain={-20}>
-            <Osc {...this.props} freq={60} />
-          </BQFilter>
-        </Out>
+        <Out0 {...this.props}>
+          <BQFilter0 {...this.props} type='highshelf' freq={300} gain={-25}>
+            <Distortion0 {...this.props}>
+              <Distortion0 {...this.props}>
+                <BQFilter0 {...this.props} type='lowshelf' freq={100} gain={-10}>
+                  <Osc0 {...this.props} freq={40} type='triangle' />
+                </BQFilter0>
+              </Distortion0>
+            </Distortion0>
+          </BQFilter0>
+        </Out0>
       </div>
     );
   }
@@ -75,7 +109,4 @@ let Audio = React.createClass({
   }
 });
 
-ReactDOM.render(
-  <Audio />,
-  document.getElementById('content')
-);
+ReactDOM.render(<Audio />, document.getElementById('content'));
