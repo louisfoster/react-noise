2dd44f655908b05e947c53e544481b7b28794c93
Louis Foster <123@louisfoster.com>
2016-10-14 16:10:59 +1100

    added noise

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,40 @@ function inOut(input, output) {
   });
 }
 
+let Gain0 = React.createClass({
+  render: function() {
+    let gain = this.props.context.createGain();
+    gain.gain.value = 15;
+    let inputs = inOut(this.props.children, gain);
+    gain.connect(this.props.out);
+    return (
+      <div>
+        <p>Attached gain</p>
+        {inputs}
+      </div>
+    );
+  }
+});
+
+let Compressor0 = React.createClass({
+  render: function() {
+    let compressor = this.props.context.createDynamicsCompressor();
+    compressor.threshold.value = -50;
+    compressor.knee.value = 13;
+    compressor.ratio.value = 15;
+    compressor.attack.value = 0;
+    compressor.release.value = 1;
+    let inputs = inOut(this.props.children, compressor);
+    compressor.connect(this.props.out);
+    return (
+      <div>
+        <p>Attached compressor</p>
+        {inputs}
+      </div>
+    );
+  }
+});
+
 let Distortion0 = React.createClass({
   render: function() {
     let distort = this.props.context.createWaveShaper();
@@ -66,6 +100,32 @@ let Osc0 = React.createClass({
   }
 })
 
+let WhiteNoise = React.createClass({
+  render: function() {
+    const channels = 2;
+    const frameCount = this.props.context.sampleRate * 0.1;
+    const arrayBuffer = this.props.context.createBuffer(channels, frameCount,
+      54321);
+    for (let c = 0; c < channels; c++) {
+       let buffering = arrayBuffer.getChannelData(c);
+       for (let i = 0; i < frameCount; i++) {
+         buffering[i] = (Math.random() * 2 - 1) * 0.485;
+       }
+    }
+    let source = this.props.context.createBufferSource();
+    source.buffer = arrayBuffer;
+    source.loop = true;
+    source.detune.value = 1845;
+    source.connect(this.props.out);
+    source.start();
+    return (
+      <div>
+        <p>Generating white noise</p>
+      </div>
+    )
+  }
+});
+
 let Out0 = React.createClass({
   render: function() {
     let dest = this.props.context.destination;
@@ -85,15 +145,35 @@ let Context = React.createClass({
       <div>
         <p>Audio context rendered</p>
         <Out0 {...this.props}>
-          <BQFilter0 {...this.props} type='highshelf' freq={300} gain={-25}>
-            <Distortion0 {...this.props}>
+          <Gain0 {...this.props}>
+            <Compressor0 {...this.props}>
               <Distortion0 {...this.props}>
-                <BQFilter0 {...this.props} type='lowshelf' freq={100} gain={-10}>
-                  <Osc0 {...this.props} freq={40} type='triangle' />
+                <BQFilter0 {...this.props} type='highshelf' freq={550} gain={-35}>
+                  <Compressor0 {...this.props}>
+                    <Distortion0 {...this.props}>
+                      <Compressor0 {...this.props}>
+                        <Distortion0 {...this.props}>
+                          <BQFilter0 {...this.props} type='lowshelf' freq={440} gain={5}>
+                              <BQFilter0 {...this.props} type='lowshelf' freq={40} gain={-35}>
+                                <Osc0 {...this.props} freq={8} type='triangle' />
+                              </BQFilter0>
+                            <BQFilter0 {...this.props} type='lowpass' freq={320} gain={2}>
+                              <BQFilter0 {...this.props} type='lowpass' freq={600} gain={4}>
+                                <BQFilter0 {...this.props} type='lowpass' freq={1000} gain={6}>
+                                  <WhiteNoise {...this.props} />
+                                  <WhiteNoise {...this.props} />
+                                </BQFilter0>
+                              </BQFilter0>
+                            </BQFilter0>
+                          </BQFilter0>
+                        </Distortion0>
+                      </Compressor0>
+                    </Distortion0>
+                  </Compressor0>
                 </BQFilter0>
               </Distortion0>
-            </Distortion0>
-          </BQFilter0>
+            </Compressor0>
+          </Gain0>
         </Out0>
       </div>
     );
